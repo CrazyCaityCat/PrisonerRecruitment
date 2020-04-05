1c414f4a5f9ffac57fddadf0e256e1340ebb8d15
CrazyCaityCat <62861606+CrazyCaityCat@users.noreply.github.com>
2020-04-05 10:15:59 -0700

    Add files via upload

---
+++ PrisonerRecruitmentModTest.cs
@@ -0,0 +1,16 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+
+
+public class DefaultPrisonerRecruitmentCalculationModel
+{
+    public static int GetConformityChangePerHour(PartyBase party)
+    {
+        return 5;
+    }
+}
+
+
+
